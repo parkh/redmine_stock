c17870eddd0c6f7cbd4885c441ba03d9a3516b2c
Dan Parkhomenko <danylo.parkhomenko@masterofcode.com>
2014-02-23 17:09:36 +0200

    init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_stock do
+  name 'Redmine Stock plugin'
+  author 'Dan Parkh'
+  description 'This is a stock plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'mailto:da.parho@gmail.com'
+end
